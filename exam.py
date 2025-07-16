de211eb9c739ac120d3cc288e430cf51decaeab3
MonsterWison <monsterbb100@gmail.com>
2025-07-16 20:58:55 +0800

    Exam-main-point, exam.py

---
+++ exam.py
@@ -0,0 +1,35 @@
+print("case 1")
+if 0 :
+    print("Hello World")
+elif 1 :
+    print("second ture")
+else:
+    print("false")
+print("out of if condition")
+
+print("case 2")
+if [] :
+    print("Hello World")
+elif 1 :
+    print("second ture")
+else:
+    print("false")
+print("out of if condition")
+
+print("case 3")
+if [1] :
+    print("Hello World")
+elif 1 :
+    print("second ture")
+else:
+    print("false")
+print("out of if condition")
+
+print("case 4")
+if [0] :
+    print("Hello World")
+elif 1 :
+    print("second ture")
+else:
+    print("false")
+print("out of if condition")
